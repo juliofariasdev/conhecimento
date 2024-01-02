46e26f1377e1b9af7ee32bbbaef97d7e62715ff1
juliofariasdev <jjuliossilva48@gmail.com>
2024-01-02 18:07:14 -0300

    falta orientação a objeto para continuar.

--- Computacao/PYTHON/projeto_banco/gerencia_dos_dados.py
+++ Computacao/PYTHON/projeto_banco/gerencia_dos_dados.py
@@ -1,41 +1,42 @@
-def banco():
+from main import login
 
-    with open('dados.txt') as arquivo:
-        lines = arquivo.readlines()
+with open('dados.txt') as arquivo:
+    lines = arquivo.readlines()
 
-    def gerenciador(linhas):
-        perfis = {}
-        for linha in linhas:
-            dados = linha.split(';')
-            if '\n' in dados[3]:
-                dados[3] = dados[3][:-1]
-            perfis[dados[0]] = {
-                'tipo': dados[1],
-                'senha': dados[2],
-                'valor': float(dados[3])
-            }
-        return perfis
-
-
-    def validador_de_login(usuario_passado,senha_passada):
-        if usuario_passado in perfis_validos:
-            if senha_passada == perfis_validos[usuario_passado]['senha']:
-                return True
-            else:
-                return False
+def gerenciador(linhas):
+    perfis = {}
+    for linha in linhas:
+        dados = linha.split(';')
+        if '\n' in dados[3]:
+            dados[3] = dados[3][:-1]
+        perfis[dados[0]] = {
+            'tipo': dados[1],
+            'senha': dados[2],
+            'valor': float(dados[3])
+        }
+    return perfis
 
 
-    def transferencia(usuario_enviador,usuario_recebedor,quantia):
-        perfis_validos[usuario_enviador]['valor'] -= quantia
-        perfis_validos[usuario_recebedor]['valor'] += quantia
+def validador_de_login(usuario_passado,senha_passada):
+    if usuario_passado in perfis_validos:
+        if senha_passada == perfis_validos[usuario_passado]['senha']:
+            return True
+        else:
+            return False
 
 
-    def sacar(usuario_sacador,saque):
-        perfis_validos[usuario_sacador]['valor'] -= saque
+def transferencia(usuario_enviador,usuario_recebedor,quantia):
+    perfis_validos[usuario_enviador]['valor'] -= quantia
+    perfis_validos[usuario_recebedor]['valor'] += quantia
 
 
-    perfis_validos = gerenciador(lines)
-    print(perfis_validos)
-    sacar('joao', 1000)
+def sacar(usuario_sacador,saque):
+    perfis_validos[usuario_sacador]['valor'] -= saque
 
-    print(perfis_validos)
+perfis_validos = gerenciador(lines)
+'''
+perfis_validos = gerenciador(lines)
+print(perfis_validos)
+sacar('joao', 1000)
+print(perfis_validos)'''
+validador_de_login(login())
