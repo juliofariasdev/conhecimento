69d2234885089e80ac2410b346fa30cedcea8314
juliofariasdev <jjuliossilva48@gmail.com>
2024-01-05 16:18:46 -0300

    teoria sobre 'funções de ordem maior'.

---
+++ Computacao/PYTHON/guppe/teoria/funcao_de_ordem_maior.py
@@ -0,0 +1,17 @@
+"""
+Funçõa de Ordem Maior - high Orde function - hof
+
+# Nada mais que uma função passada como argumento
+
+function(funcao)
+
+Cidadãos de primeira classe -fcc
+
+# uma nested function
+
+def funcao1():
+    def funcao2():
+        acao
+        return
+    return funcao2()
+"""
