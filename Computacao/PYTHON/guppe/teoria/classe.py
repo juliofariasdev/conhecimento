308bed2d2bb846429577dc257b9978bd556d3c3e
juliofariasdev <jjuliossilva48@gmail.com>
2024-01-06 17:56:24 -0300

    teoria sobre 'classe'.

---
+++ Computacao/PYTHON/guppe/teoria/classe.py
@@ -0,0 +1,14 @@
+"""
+Em POO são nada mais nada menos que modelos de objetos do mundo real
+representados computacionalmente.
+
+pass - > usamos quando queremos fazer um bloco de código sem implentação de nada.
+
+class Classe:
+    pass
+
+OBS: Nossas classes devem ser definidas com inicial maiúscula.
+
+entidade do sistema = classe
+
+"""
