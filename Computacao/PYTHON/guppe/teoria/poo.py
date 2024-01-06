b07c64c05fb41221b08deeb53e1fbfb16f7040f8
juliofariasdev <jjuliossilva48@gmail.com>
2024-01-06 10:30:10 -0300

    teoria sobre 'POO'.

---
+++ Computacao/PYTHON/guppe/teoria/poo.py
@@ -0,0 +1,17 @@
+"""
+Programação Orientada a Objetos - POO
+
+- POO é um paradigma de programação que utiliza do mapeamento
+de objetos do mundo real para modelos computacionais.
+
+- Paradigma na programação é a forma/metodologia utilizada para
+pensar/desenvolver um sistema.
+
+Principais elemento de POO:
+Classe -> Modelo de objeto
+Atributo -> Características do objeto
+Método -> Comportamento do Objeto (funções)
+Construtor -> Métodos especiais para criar um objeto
+Objeto -> instância da class
+
+"""
