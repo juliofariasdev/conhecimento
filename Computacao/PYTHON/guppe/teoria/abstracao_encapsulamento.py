dc7a07ba2294334e735aacae05f6314ab7f5e85f
juliofariasdev <jjuliossilva48@gmail.com>
2024-01-08 15:30:35 -0300

    teoria sobre 'abstraçÃão e encapsulamento'.

---
+++ Computacao/PYTHON/guppe/teoria/abstracao_encapsulamento.py
@@ -0,0 +1,7 @@
+"""
+POO - Abstração e Encapsulamento
+
+Abstração -> Isolamento da informaçãoe necessarias  enttre o sistema e o usuário
+
+# Encapsulamento -> Isolamento dos dados de um sistema para a assim manter a segurança do mesmo.
+"""
